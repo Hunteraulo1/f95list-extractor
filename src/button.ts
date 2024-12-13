394a2d28b2a23cd7bb39ac50189a0e8c22c33b91
Hunteraulo1 <hunteraulo@pm.me>
2024-12-13 10:24:48 +0100

    :lipstick: fix rem to px

--- src/button.ts
+++ src/button.ts
@@ -3,18 +3,18 @@ import { panelElement } from "./panel";
 const openElement = document.createElement("button");
 
 export const button = () => {
-	openElement.style.height = "4rem";
-	openElement.style.width = "4rem";
+	openElement.style.height = "48px";
+	openElement.style.width = "48px";
 	openElement.style.borderRadius = "99999px";
 	openElement.style.backgroundColor = "#1e2022";
 	openElement.style.position = "fixed";
-	openElement.style.top = "2rem";
-	openElement.style.right = "2rem";
+	openElement.style.top = "16px";
+	openElement.style.right = "16px";
 	openElement.style.zIndex = "1000";
 	openElement.style.border = "none";
 	openElement.style.color = "#ba4545";
 	openElement.style.fontWeight = "bold";
-	openElement.style.fontSize = "2rem";
+	openElement.style.fontSize = "24px";
 	openElement.textContent = "E";
 
 	const body = document.querySelector("body");
--- src/panel.ts
+++ src/panel.ts
@@ -3,12 +3,12 @@ import { extractData, extractTags } from "./actions";
 export const panelElement = document.createElement("div");
 
 export const panel = () => {
-	panelElement.style.width = "16rem";
-	panelElement.style.borderRadius = "1rem";
+	panelElement.style.width = "128px";
+	panelElement.style.borderRadius = "8px";
 	panelElement.style.backgroundColor = "#1e2022";
 	panelElement.style.position = "fixed";
-	panelElement.style.top = "2rem";
-	panelElement.style.right = "2rem";
+	panelElement.style.top = "16px";
+	panelElement.style.right = "16px";
 	panelElement.style.zIndex = "1001";
 	panelElement.style.display = "none";
 	panelElement.style.flex = "flex";
@@ -27,15 +27,15 @@ export const panel = () => {
 const closeButton = (element: HTMLDivElement) => {
 	const close = document.createElement("button");
 
-	close.style.height = "2rem";
-	close.style.width = "2rem";
+	close.style.height = "16px";
+	close.style.width = "16px";
 	close.style.borderRadius = "99999px";
 	close.style.backgroundColor = "#37383a";
 	close.style.alignSelf = "end";
 	close.style.border = "none";
 	close.style.color = "#ba4545";
 	close.style.fontWeight = "bold";
-	close.style.fontSize = "1rem";
+	close.style.fontSize = "8px";
 	close.textContent = "X";
 
 	element.prepend(close);
@@ -52,24 +52,28 @@ const handleClickClose = (element: HTMLButtonElement) => {
 const button = (title: string, action: () => string) => {
 	const button = document.createElement("button");
 
-	button.style.height = "2rem";
+	button.style.height = "16px";
 	button.style.width = "100%";
 	button.style.backgroundColor = "#37383a";
 	button.style.border = "#ba4545";
-	button.style.borderRadius = "1rem";
+	button.style.borderRadius = "8px";
 	button.style.color = "#ba4545";
 	button.style.fontWeight = "bold";
-	button.style.fontSize = "1rem";
+	button.style.fontSize = "8px";
 	button.textContent = title;
 
 	panelElement.append(button);
 
-	handleClickButton(action);
+	button.addEventListener("click", () => {
+		handleClickButton(action);
+	});
 };
 
 const handleClickButton = (action: () => string) => {
+	const text = action();
+
 	navigator.clipboard
-		.writeText(action())
+		.writeText(text)
 		.then(() => console.log("Text copied to clipboard"))
 		.catch((err) => console.error("Could not copy text", err));
 };
